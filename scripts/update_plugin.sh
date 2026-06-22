6654675f6c19b6fd34b09fdfd0128c8b7876b785
Hussain Arif <hussainarifkl@gmail.com>
2026-06-22 13:54:41 +0200

    Update plugin hash

--- scripts/update_plugin.sh
+++ scripts/update_plugin.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-git add . && git commit -m "Update plugin hash" && git pull && git rebase &&  git push
+git pull && git add . && git commit -m "Update plugin hash" && git pull && git rebase &&  git push
